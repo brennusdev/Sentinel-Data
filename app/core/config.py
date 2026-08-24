183e99358118056e282bcf5c5b6ed08da5c58bb0
Brenno Pereira <brennuscontato@gmail.com>
2026-08-24 18:27:47 -0300

    config atualizado
    Parte de atualização v4

--- app/core/config.py
+++ app/core/config.py
@@ -7,7 +7,7 @@ import os
 
 class Settings:
     """
-    Armazena as configurações da aplicação.
+    Armazena configurações da aplicação.
     """
 
     APP_NAME: str = os.getenv(
@@ -25,25 +25,31 @@ class Settings:
         "postgresql+psycopg://sentinel:sentinel@localhost:5432/sentinel",
     )
 
-    # Endereço do Kafka.
     KAFKA_BOOTSTRAP_SERVERS: str = os.getenv(
         "KAFKA_BOOTSTRAP_SERVERS",
         "kafka:9092",
     )
 
-    # Topic principal dos eventos.
     KAFKA_EVENTS_TOPIC: str = os.getenv(
         "KAFKA_EVENTS_TOPIC",
         "events.raw",
     )
 
-    # Topic para eventos que falharam.
+    KAFKA_PROCESSED_TOPIC: str = os.getenv(
+        "KAFKA_PROCESSED_TOPIC",
+        "events.processed",
+    )
+
+    KAFKA_INVALID_TOPIC: str = os.getenv(
+        "KAFKA_INVALID_TOPIC",
+        "events.invalid",
+    )
+
     KAFKA_DLQ_TOPIC: str = os.getenv(
         "KAFKA_DLQ_TOPIC",
         "events.dlq",
     )
 
-    # Grupo responsável pelo processamento.
     KAFKA_CONSUMER_GROUP: str = os.getenv(
         "KAFKA_CONSUMER_GROUP",
         "sentinel-event-processors",

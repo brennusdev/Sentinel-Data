f4ed39700e579106a88ec6c6600fbf4b8b9bcef7
Brenno Pereira <brennuscontato@gmail.com>
2026-08-26 10:36:38 -0300

    Criado test_observability
    Atualização de versões v5,v6 e v7 do projeto

---
+++ tests/test_observability.py
@@ -0,0 +1,22 @@
+from observability.metrics import (
+    EVENTS_PROCESSED,
+)
+
+
+def test_processed_metric():
+
+    before = (
+        EVENTS_PROCESSED
+        ._value
+        .get()
+    )
+
+    EVENTS_PROCESSED.inc()
+
+    after = (
+        EVENTS_PROCESSED
+        ._value
+        .get()
+    )
+
+    assert after == before + 1

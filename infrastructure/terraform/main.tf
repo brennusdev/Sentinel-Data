be11c9b1c96cdfccb73477a2d6e5bbf3bc835e1f
Brenno Pereira <brennuscontato@gmail.com>
2026-08-26 11:15:49 -0300

    Criado main.tf
    Atualização v9 do projeto Sentinel Data

---
+++ infrastructure/terraform/main.tf
@@ -0,0 +1,29 @@
+resource "docker_container" "sentinel_api" {
+
+  name = "sentinel-api"
+
+  image = docker_image.sentinel.image_id
+
+  ports {
+
+    internal = 8000
+
+    external = 8000
+  }
+
+  env = [
+
+    "APP_ENV=production"
+  ]
+}
+resource "docker_image" "sentinel" {
+
+  name = "sentinel-data:latest"
+
+  build {
+
+    context = "../.."
+
+    dockerfile = "docker/Dockerfile"
+  }
+}
